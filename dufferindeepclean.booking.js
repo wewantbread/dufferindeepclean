f4d4a4a3f7d6c2a704f685bf03954a8508794b0b
marth <dufferindeepcleaners@gmail.com>
2025-08-09 18:31:48 -0400

    Create dufferindeepclean.booking.js

---
+++ dufferindeepclean.booking.js
@@ -0,0 +1,8 @@
+<!-- booking wizard JS -->
+<script>
+  (function(){
+    const form = document.getElementById('bookingForm');
+    const steps = Array.from(document.querySelectorAll('.step'));
+    ...
+  })();
+</script>
